0bd4d60509f2474ac636615b39c3974075749f27
Pasindu Pramodh <73321082+Pasindupramodh@users.noreply.github.com>
2024-09-25 15:06:30 +0530

    animation

--- submit_quiz.php
+++ submit_quiz.php
@@ -1,4 +1,5 @@
 <?php
+include('config.php');
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $question1 = $_POST['question1'] ?? null;
@@ -131,19 +132,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
 
-    $host = 'localhost';
-    $port = '3309'; // Replace with your port number if different
-    $db = 'ch';
-    $user = 'root';
-    $pass = '12345';
-    $charset = 'utf8mb4';
-
-    $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
-    $options = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
-    ];
+  
     $logEntry = "name: " . $name . "\n";
     $logEntry .= "mobile: " . $mobile . "\n";
     $logEntry .= "\n\nanswers: " . $ans . "\n";
