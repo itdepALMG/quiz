a7ffb08df2277bd6be0468df14a2ebef05886164
itdepALMG <itdep090@gmail.com>
2024-09-30 12:23:35 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
   $host = 'localhost';
   $port = '3309'; // Replace with your port number if different
-  $db = 'ch';
-  $user = 'root';
-  $pass = '12345';
+  $db = '';
+  $user = '';
+  $pass = '';
   $charset = 'utf8mb4';
 
   $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
